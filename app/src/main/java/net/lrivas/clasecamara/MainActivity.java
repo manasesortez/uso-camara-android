77742eb4ec976545ba9eeef19f1474a0f53a7d94
manasesortez <manasesortez1234@hotmail.com>
2022-02-19 14:23:51 -0600

    uso camara

--- app/src/main/java/net/lrivas/clasecamara/MainActivity.java
+++ app/src/main/java/net/lrivas/clasecamara/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -28,94 +29,65 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnTomarFoto;
-    ImageView imgFoto;
-    String rutaImagenes;
+    Button gallery;
+    Button whatsappButton;
     private static final int REQUEST_CODIGO_CAMARA=200;
-    private static final int REQUEST_CODIGO_CAPTURAR_IMAGEN = 201;
+    Uri selectedImage;
 
+    @SuppressLint({"WrongViewCast", "ObsoleteSdkInt"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Referencias
-        btnTomarFoto = findViewById(R.id.btnTomarFoto);
-        imgFoto = findViewById(R.id.imgFoto);
+        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                }, REQUEST_CODIGO_CAMARA);
+            }
+        }
 
-        //Evento clic
-        btnTomarFoto.setOnClickListener(new View.OnClickListener() {
+        whatsappButton = findViewById(R.id.compartirWhatsapp);
+        whatsappButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                realizarTomaDeFotografia();
+                Intent intentW = new Intent(Intent.ACTION_SEND);
+                intentW.setType("image/");
+                intentW.setPackage("com.whatsapp");
+                if (selectedImage != null){
+                    intentW.putExtra(Intent.EXTRA_STREAM, selectedImage);
+                    try {
+                        startActivity(intentW);
+                    }catch(Exception e){
+                        Toast.makeText(MainActivity.this, "Error al Enviar\n"+ e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this, "No se Selecciono una Imagen", Toast.LENGTH_LONG).show();
+                }
             }
         });
-    }
 
-    public void realizarTomaDeFotografia(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-            if(ActivityCompat.checkSelfPermission(MainActivity.this,
-                    Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED){
-                    tomarFoto();
-            }else{
-                //Versiones abajo de la N
-                ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[]{Manifest.permission.CAMERA},REQUEST_CODIGO_CAMARA);
+        gallery = findViewById(R.id.Gallery);
+        gallery.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
             }
-        }else{
-           tomarFoto();
-        }
+        });
+
+
+
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == REQUEST_CODIGO_CAMARA){
-            if(permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                tomarFoto();
-            }
-        }else{
-            Toast.makeText(MainActivity.this, "Se necesitan los permisos de la camara para usar esta funcionalidad", Toast.LENGTH_SHORT).show();
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == REQUEST_CODIGO_CAPTURAR_IMAGEN){
-            //Se capturó una fotografia y se dió ok
-            if(resultCode == Activity.RESULT_OK){
-                //asignado la foto tomada al control Image
-                imgFoto.setImageURI(Uri.parse(rutaImagenes));
-            }
-        }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    public void tomarFoto(){
-        Intent intentCamara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intentCamara.resolveActivity(getPackageManager())!=null){
-            File archivoFoto = null;
-            archivoFoto = generarArchivo();
-            if(archivoFoto!=null){
-                Uri rutaFoto = FileProvider.getUriForFile(MainActivity.this,
-                        "net.lrivas.clasecamara",archivoFoto);
-                intentCamara.putExtra(MediaStore.EXTRA_OUTPUT,rutaFoto);
-                startActivityForResult(intentCamara,REQUEST_CODIGO_CAPTURAR_IMAGEN);
-            }
+        if(resultCode  == RESULT_OK && data != null){
+             selectedImage = data.getData();
+            ImageView imageView = findViewById(R.id.imageView);
+            imageView.setImageURI(selectedImage);
         }
-    }
-
-    public File generarArchivo(){
-        String nomenclatura = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-        String prefijoArchivo = "APPCAM_"+nomenclatura+"_";
-        File directoriImagen = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File miImagen = null;
-        try{
-            miImagen = File.createTempFile(prefijoArchivo,".jpg",directoriImagen);
-            rutaImagenes = miImagen.getAbsolutePath();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        return miImagen;
     }
 }
